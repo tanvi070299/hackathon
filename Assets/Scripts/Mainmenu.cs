2d861e42e6c3d70e269d8df10a6a2213703d15cc
Tanvi <Tanvithakur1999@gmail.com>
2020-09-06 23:48:08 +0530

    Fin2

---
+++ Assets/Scripts/Mainmenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class Mainmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRSettings.enabled = false;
+    }
+
+    public void GotoMedMountain()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene("VRMeditation");
+    }
+    public void GotoMedForest()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene("VRMeditation2");
+    }
+
+    public void GotoPB()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene("PunchScene");
+    }
+
+    public void GotoPhobiaHeight()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene("PhobiaHeight");
+    }
+
+    public void GotoPhobiaDark()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene("PhobiaDark");
+    }
+
+    public void GotoPhobiaSpider()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene(3);
+    }
+    public void GotoBubblePop()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene("VRGame");
+    }
+
+    public void GotoAudience()
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene(5);
+    }
+
+    public void GotoTherapy()
+    {
+
+        SceneManager.LoadScene(6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SoundSensivity.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SoundSensivity : MonoBehaviour
+{
+    public float sensivity = 0;
+    public float loudness = 0;
+
+    public Text loudLevel;
+    public Text action;
+    AudioSource audioS;
+    private int hi;
+    float lou;
+    static Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lou = 0;
+        hi = 0;
+        audioS = GetComponent<AudioSource>();
+        audioS.clip = Microphone.Start(null, true, 10, 44100);
+        audioS.loop = true;
+        audioS.mute = false;
+        anim = GetComponent<Animator>();
+        while (!(Microphone.GetPosition(null) > 0)) {
+
+            //Debug.Log("No Mic");
+        }
+        audioS.Play();
+
+        audioS.volume = 0.01f;
+        //aduioS.Mute = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loudness = GetAvgVol() * sensivity;
+        if(loudness>lou)
+        {
+            lou = loudness;
+        }
+        Debug.Log(lou + "+"+loudness);
+        //  ToS
+        // to
+       // _ShowAndroidToastMessage(loudness.ToString());
+        // Debug.Log("Loudness :" + loudness);
+        loudLevel.text="Loudness level +"+loudness.ToString();
+        if (loudness > 0.031 && loudness<0.081)     //Mild
+        {
+            //  this.GetComponent<>;
+         //   _ShowAndroidToastMessage("Loud AF");
+         
+            action.text="Hit loudness";
+            anim.SetTrigger("Hit");
+            Debug.Log("Sad Nigga hours");
+            hi++;
+            Debug.Log(hi);
+        }
+
+        if (loudness > 0.081 || hi>14)     //Mild
+        {
+            int yy;
+            //  this.GetComponent<>;
+        //    _ShowAndroidToastMessage("Loud AF");
+        
+            action.text="Dead loudness";
+            anim.SetTrigger("dead");
+            Debug.Log("dead Nigga hours");
+            Debug.Log(loudness);
+            StartCoroutine(resetTheSCene());
+        
+            hi++;
+            Debug.Log(hi);
+            //scenefucks();
+        }
+
+	
+    }
+
+    IEnumerator resetTheSCene()
+    {
+        yield return new WaitForSeconds(3);        
+            action.text="Restting Now";
+            hi = 0;
+			Scene scene = SceneManager.GetActiveScene();
+			SceneManager.LoadScene(scene.name);
+		
+
+    }
+
+    float GetAvgVol()
+    {
+        float[] data = new float[256];
+        float a = 0;
+        audioS.GetOutputData(data, 0);
+        foreach(float s in data)
+        {
+            a += Mathf.Abs(s);
+         //   Debug.Log("Value of a :" + a);
+        }
+        return a / 256;
+    }
+
+	private void scenefucks()
+	{
+		
+		
+
+	}
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity =
+            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject =
+                    toastClass.CallStatic<AndroidJavaObject>(
+                        "makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
